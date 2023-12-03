2cefe355cc9854012881969f237931f696fc1586
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-03 16:35:16 +0900

    🎉

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const mode = process.env.NODE_ENV || 'development'
+
+const webpackConfig = {
+  mode,
+  entry: {
+    popup: path.resolve(__dirname, 'src/popup.ts')
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist/js'),
+    filename: '[name].js'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: '..' }]
+    })
+  ],
+
+  devtool: false
+}
+
+module.exports = webpackConfig
