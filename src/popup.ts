78397d75c39e70091621f15761ceadae82184c25
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-08 01:09:17 +0900

    ✨ feat: Impl chnage screen

--- src/popup.ts
+++ src/popup.ts
@@ -1,2 +1,24 @@
 const PEWSButton = document.getElementById('PEWSOpenButton')
 PEWSButton?.addEventListener('click', () => { window.open('https://www.weather.go.kr/pews/') })
+
+function changeToMainScreen (): void {
+  const defaultScreen = document.getElementById('default-screen')
+  const mainScreen = document.getElementById('main-screen')
+
+  defaultScreen?.classList.add('hidden')
+  mainScreen?.classList.remove('hidden')
+}
+
+function changeToDefaultScreen (): void {
+  const defaultScreen = document.getElementById('default-screen')
+  const mainScreen = document.getElementById('main-screen')
+
+  defaultScreen?.classList.remove('hidden')
+  mainScreen?.classList.add('hidden')
+}
+
+function chromeMessageListener (message: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void): void {
+
+}
+
+chrome.runtime.onMessage.addListener(chromeMessageListener)
