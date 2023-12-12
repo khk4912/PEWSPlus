78222336463e372d37cdf445a4ea52f2da454518
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-12 16:33:50 +0900

    🩹 fix: Fix line length warning

--- src/utils/chrome_message.ts
+++ src/utils/chrome_message.ts
@@ -1,12 +1,27 @@
 import type { MessageDataInterface, MessageInterface } from '../types/message'
 
-export async function sendMessage (runtime: typeof chrome.runtime, event: 'init', data?: undefined): Promise<void>
-export async function sendMessage<T extends keyof MessageDataInterface, X extends MessageDataInterface[T]> (runtime: typeof chrome.runtime, event: T, data: X): Promise<void> {
+export async function sendMessage (
+  runtime: typeof chrome.runtime | typeof window,
+  event: 'init', data?: undefined,
+): Promise<void>
+export async function sendMessage
+  <T extends keyof MessageDataInterface, X extends MessageDataInterface[T]> (
+  runtime: typeof chrome.runtime | typeof window,
+  event: T,
+  data: X
+): Promise<void> {
   const msg: MessageInterface = { event }
 
   if (data !== undefined) {
     msg.data = data
   }
+
+  if (runtime === window) {
+    window.postMessage({ type: 'PEWSWatcher', msg }, '*')
+    return
+  }
+
+  runtime = runtime as typeof chrome.runtime
   await runtime.sendMessage(msg)
 }
 
